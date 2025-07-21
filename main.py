37af0656e94b51a304e3c2f6f18c8347db144258
JoeCool2025 <adinjennings@gmail.com>
2025-07-21 00:05:50 -0400

    main.py updated to add sys.argv for distinct book analysis

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from stats import book_word_count
 from stats import count_book_characters
 from stats import sort_character_count
+import sys
 
-path_to_file = "/home/adin-admin/boot_dev/bookbot/books/frankenstein.txt"
-characters_used = count_book_characters(path_to_file)
+path_to_file = ""
+
+try:
+    path_to_file = sys.argv[1]
+except Exception as e:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
+    characters_used = count_book_characters(path_to_file)
+
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_file}...")
     print("----------- Word Count ----------")
     print(f"Found {book_word_count(path_to_file)} total words")
     print("--------- Character Count -------")
